fbe8318ddbfe953efee847c694564fa67f8eac68
starkre <gulkin.igor.developer@gmail.com>
2025-07-15 21:46:50 +0700

    Думаем!

--- Assets/Plugins/Atomic/Elements/Scripts/Reactive/Subscription.cs
+++ Assets/Plugins/Atomic/Elements/Scripts/Reactive/Subscription.cs
@@ -2,75 +2,86 @@ using System;
 
 namespace Atomic.Elements
 {
-    public sealed class Subscription : IDisposable
+    public readonly struct Subscription
     {
-        private readonly IReactive observable;
+        private readonly IReactive reactive;
         private readonly Action action;
 
-        public Subscription(IReactive observable, Action action)
+        public static Subscription Create(in IReactive reactive, in Action action)
         {
-            this.observable = observable;
+            if (reactive == null)
+                throw new ArgumentNullException(nameof(reactive));
+
+            if (action == null)
+                throw new ArgumentNullException(nameof(action));
+
+            reactive.Subscribe(action);
+            return new Subscription(reactive, action);
+        }
+
+        private Subscription(in IReactive reactive, in Action action)
+        {
+            this.reactive = reactive;
             this.action = action;
-            this.observable.Subscribe(action);
         }
 
         public void Dispose()
         {
-            this.observable.Unsubscribe(this.action);
+            this.reactive.Unsubscribe(this.action);
         }
     }
 
-    public sealed class Subscription<T> : IDisposable
+    public readonly struct Subscription<T> : IDisposable
     {
-        private readonly IReactive<T> observable;
+        private readonly IReactive<T> reactive;
         private readonly Action<T> action;
 
-        public Subscription(IReactive<T> observable, Action<T> action)
+        public Subscription(IReactive<T> reactive, Action<T> action)
         {
-            this.observable = observable;
+            this.reactive = reactive;
             this.action = action;
-            this.observable.Subscribe(action);
+            this.reactive.Subscribe(action);
         }
 
         public void Dispose()
         {
-            this.observable.Unsubscribe(this.action);
+            this.reactive.Unsubscribe(this.action);
         }
     }
-    
-    public sealed class Subscription<T1, T2> : IDisposable
+
+    public readonly struct Subscription<T1, T2> : IDisposable
     {
-        private readonly IReactive<T1, T2> observable;
+        private readonly IReactive<T1, T2> reactive;
         private readonly Action<T1, T2> action;
 
-        public Subscription(IReactive<T1, T2> observable, Action<T1, T2> action)
+        public Subscription(IReactive<T1, T2> reactive, Action<T1, T2> action)
         {
-            this.observable = observable;
+            this.reactive = reactive;
             this.action = action;
-            this.observable.Subscribe(action);
+            this.reactive.Subscribe(action);
         }
 
         public void Dispose()
         {
-            this.observable.Unsubscribe(this.action);
+            this.reactive.Unsubscribe(this.action);
         }
     }
-    
-    public sealed class Subscription<T1, T2, T3> : IDisposable
+
+    public readonly struct Subscription<T1, T2, T3> : IDisposable
     {
-        private readonly IReactive<T1, T2, T3> observable;
+        private readonly IReactive<T1, T2, T3> reactive;
         private readonly Action<T1, T2, T3> action;
 
-        public Subscription(IReactive<T1, T2, T3> observable, Action<T1, T2, T3> action)
+        public Subscription(IReactive<T1, T2, T3> reactive, Action<T1, T2, T3> action)
         {
-            this.observable = observable;
+            this.reactive = reactive;
             this.action = action;
-            this.observable.Subscribe(action);
+            this.reactive.Subscribe(action);
         }
 
         public void Dispose()
         {
-            this.observable.Unsubscribe(this.action);
+            this.reactive.Unsubscribe(this.action);
         }
     }
 }
