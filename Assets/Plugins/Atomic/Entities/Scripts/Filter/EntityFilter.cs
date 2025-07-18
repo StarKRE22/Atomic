0ac8e4846cf85f09574ba2371bf978435307bb9a
starkre <gulkin.igor.developer@gmail.com>
2025-07-19 01:55:10 +0700

    Обновил фильтр!

--- Assets/Plugins/Atomic/Entities/Scripts/Filter/EntityFilter.cs
+++ Assets/Plugins/Atomic/Entities/Scripts/Filter/EntityFilter.cs
@@ -7,23 +7,40 @@ using Sirenix.OdinInspector;
 
 namespace Atomic.Entities
 {
+    /// <summary>
+    /// Represents a dynamic filter over a set of entities in the world, based on a predicate and optional triggers.
+    /// Automatically tracks entities that satisfy the predicate and invokes events on changes.
+    /// </summary>
     public class EntityFilter : IEntityFilter
     {
+        /// <inheritdoc />
         public event Action<IEntity> OnAdded;
+
+        /// <inheritdoc />
         public event Action<IEntity> OnDeleted;
 
+        /// <inheritdoc />
         public int Count => entities.Count;
 
+        /// <summary>
+        /// The set of entities currently matching the filter.
+        /// </summary>
 #if ODIN_INSPECTOR
         [ShowInInspector, ReadOnly]
 #endif
         private readonly HashSet<IEntity> entities = new();
 
         private readonly IEntityWorld world;
-
         private readonly Predicate<IEntity> predicate;
         private readonly IEntityFilter.ITrigger[] triggers;
 
+        /// <summary>
+        /// Creates a new entity filter that watches a world for entities matching a predicate and responds to trigger changes.
+        /// </summary>
+        /// <param name="world">The world to observe.</param>
+        /// <param name="predicate">The predicate used to filter entities.</param>
+        /// <param name="triggers">Optional triggers that can invalidate entities or cause re-evaluation.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="world"/> or <paramref name="predicate"/> is null.</exception>
         public EntityFilter(
             in IEntityWorld world,
             in Predicate<IEntity> predicate,
@@ -36,6 +53,9 @@ namespace Atomic.Entities
             this.Initialize();
         }
 
+        /// <summary>
+        /// Initializes the filter by subscribing to existing entities and world events.
+        /// </summary>
         private void Initialize()
         {
             this.world.OnAdded += this.Subscribe;
@@ -45,6 +65,7 @@ namespace Atomic.Entities
                 this.Subscribe(entity);
         }
 
+        /// <inheritdoc />
         public void Dispose()
         {
             foreach (IEntity entity in this.world)
@@ -54,42 +75,39 @@ namespace Atomic.Entities
             this.world.OnDeleted -= this.Unsubscribe;
         }
 
+        /// <inheritdoc />
         public IEntity[] GetAll()
         {
-            var result = new IEntity[this.entities.Count];
+            IEntity[] result = new IEntity[this.entities.Count];
             this.entities.CopyTo(result);
             return result;
         }
 
+        /// <inheritdoc />
         public int GetAll(IEntity[] results)
         {
             this.entities.CopyTo(results);
             return this.entities.Count;
         }
 
+        /// <inheritdoc />
         public void CopyTo(ICollection<IEntity> results)
         {
             results.Clear();
-
-            foreach (IEntity entity in entities)
+            
+            foreach (IEntity entity in this.entities)
                 results.Add(entity);
         }
 
-        public bool Has(IEntity entity)
-        {
-            return this.entities.Contains(entity);
-        }
+        /// <inheritdoc />
+        public bool Has(IEntity entity) => this.entities.Contains(entity);
 
-        public IEnumerator<IEntity> GetEnumerator()
-        {
-            return this.entities.GetEnumerator();
-        }
+        /// <inheritdoc />
+        public IEnumerator<IEntity> GetEnumerator() => this.entities.GetEnumerator();
 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return this.GetEnumerator();
-        }
-
+        /// <inheritdoc />
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+        
         private void Subscribe(IEntity entity)
         {
             entity.OnTagAdded += this.OnTagAdded;
@@ -99,8 +117,11 @@ namespace Atomic.Entities
             entity.OnValueDeleted += this.OnValueDeleted;
             entity.OnValueChanged += this.OnValueChanged;
 
-            foreach (var trigger in triggers)
+            for (int i = 0, count = this.triggers.Length; i < count; i++)
+            {
+                IEntityFilter.ITrigger trigger = this.triggers[i];
                 trigger.Subscribe(entity, this.Synchronize);
+            }
 
             if (this.predicate(entity) && this.entities.Add(entity))
                 this.OnAdded?.Invoke(entity);
@@ -115,8 +136,11 @@ namespace Atomic.Entities
             entity.OnValueDeleted -= this.OnValueDeleted;
             entity.OnValueChanged -= this.OnValueChanged;
 
-            foreach (var trigger in triggers)
+            for (int i = 0, count = this.triggers.Length; i < count; i++)
+            {
+                IEntityFilter.ITrigger trigger = this.triggers[i];
                 trigger.Unsubscribe(entity, this.Synchronize);
+            }
 
             if (this.entities.Remove(entity))
                 this.OnDeleted?.Invoke(entity);
@@ -132,7 +156,6 @@ namespace Atomic.Entities
         private void Synchronize(IEntity entity)
         {
             bool matches = this.predicate(entity);
-
             if (!matches && this.entities.Remove(entity)) this.OnDeleted?.Invoke(entity);
             else if (matches && this.entities.Add(entity)) this.OnAdded?.Invoke(entity);
         }
--- Assets/Plugins/Atomic/Entities/Scripts/Filter/IEntityFilter.cs
+++ Assets/Plugins/Atomic/Entities/Scripts/Filter/IEntityFilter.cs
@@ -3,25 +3,77 @@ using System.Collections.Generic;
 
 namespace Atomic.Entities
 {
-    /**
-     * Experimental.
-     */
+    /// <summary>
+    /// Defines a filtered view over a collection of entities,
+    /// supporting change tracking, querying, and data copying.
+    /// </summary>
     public interface IEntityFilter : IDisposable, IEnumerable<IEntity>
     {
+        /// <summary>
+        /// Occurs when an entity enters the filter (i.e., matches the filter criteria).
+        /// </summary>
         event Action<IEntity> OnAdded;
+
+        /// <summary>
+        /// Occurs when an entity is removed from the filter (i.e., no longer matches the criteria).
+        /// </summary>
         event Action<IEntity> OnDeleted;
 
+        /// <summary>
+        /// Gets the number of entities currently included in the filter.
+        /// </summary>
         int Count { get; }
-        
+
+        /// <summary>
+        /// Determines whether the specified entity is currently present in the filter.
+        /// </summary>
+        /// <param name="entity">The entity to check.</param>
+        /// <returns><c>true</c> if the entity is contained in the filter; otherwise, <c>false</c>.</returns>
         bool Has(IEntity entity);
+
+        /// <summary>
+        /// Returns a new array containing all entities in the filter.
+        /// </summary>
+        /// <returns>An array of all filtered entities.</returns>
         IEntity[] GetAll();
+
+        /// <summary>
+        /// Copies all filtered entities into the provided array.
+        /// </summary>
+        /// <param name="results">The array to populate.</param>
+        /// <returns>The number of entities copied.</returns>
         int GetAll(IEntity[] results);
+
+        /// <summary>
+        /// Copies all filtered entities into the specified collection.
+        /// </summary>
+        /// <param name="results">The collection to populate.</param>
         void CopyTo(ICollection<IEntity> results);
-        
+
+        /// <summary>
+        /// Provides a mechanism for reacting to per-entity changes that might affect filter membership.
+        /// </summary>
         public interface ITrigger
         {
-            void Subscribe(IEntity entity, Action<IEntity> callback);
-            void Unsubscribe(IEntity entity, Action<IEntity> callback);
+            /// <summary>
+            /// Delegate used for synchronizing entity state within the filter.
+            /// </summary>
+            /// <param name="entity">The affected entity.</param>
+            public delegate void SyncAction(IEntity entity);
+            
+            /// <summary>
+            /// Subscribes to entity-specific events to track relevant changes.
+            /// </summary>
+            /// <param name="entity">The entity to observe.</param>
+            /// <param name="syncAction">The callback to invoke when a tracked change occurs.</param>
+            void Subscribe(IEntity entity, SyncAction syncAction);
+
+            /// <summary>
+            /// Unsubscribes from previously tracked entity-specific changes.
+            /// </summary>
+            /// <param name="entity">The entity that was being observed.</param>
+            /// <param name="syncAction">The callback to invoke when a tracked change occurs.</param>
+            void Unsubscribe(IEntity entity, SyncAction syncAction);
         }
     }
 }
