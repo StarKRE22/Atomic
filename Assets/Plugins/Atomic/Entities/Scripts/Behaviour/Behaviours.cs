55295fd00bcd270c70e726400b094293eef6e474
starkre <gulkin.igor.developer@gmail.com>
2025-07-19 22:21:17 +0700

    Думаем!

--- Assets/Plugins/Atomic/Entities/Scripts/Behaviour/Behaviours.cs
+++ Assets/Plugins/Atomic/Entities/Scripts/Behaviour/Behaviours.cs
@@ -50,49 +50,49 @@ namespace Atomic.Entities
 
     #region Base
 
-    public interface IEntityBehaviour : IBehaviour<IEntity>
-    {
-    }
-
-    public interface IEntityInit : IInit<IEntity>
-    {
-        void Init(IEntity<E> entity);
-    }
-
-    public interface IEnable<E> : IBehaviour<E> where E : IEntity<E>
-    {
-        void Enable(IEntity<E> entity);
-    }
-
-    public interface IUpdate<E> : IBehaviour<E> where E : IEntity<E>
-    {
-        void OnUpdate(IEntity<E> entity, float deltaTime);
-    }
-
-    public interface IFixedUpdate<E> : IBehaviour<E> where E : IEntity<E>
-    {
-        void OnFixedUpdate(IEntity<E> entity, float deltaTime);
-    }
-
-    public interface ILateUpdate<E> : IBehaviour<E> where E : IEntity<E>
-    {
-        void OnLateUpdate(IEntity<E> entity, float deltaTime);
-    }
-
-    public interface IDisable<E> : IBehaviour<E> where E : IEntity<E>
-    {
-        void Disable(IEntity<E> entity);
-    }
-
-    public interface IDispose<E> where E : IEntity<E>
-    {
-        void Dispose(IEntity<E> entity);
-    }
-
-    public interface IGizmos<E> : IBehaviour<E> where E : IEntity<E>
-    {
-        void OnGizmosDraw(IEntity<E> entity);
-    }
+    // public interface IEntityBehaviour : IBehaviour<IEntity>
+    // {
+    // }
+    //
+    // public interface IEntityInit : IInit<IEntity>
+    // {
+    //     void Init(IEntity<E> entity);
+    // }
+    //
+    // public interface IEnable<E> : IBehaviour<E> where E : IEntity<E>
+    // {
+    //     void Enable(IEntity<E> entity);
+    // }
+    //
+    // public interface IUpdate<E> : IBehaviour<E> where E : IEntity<E>
+    // {
+    //     void OnUpdate(IEntity<E> entity, float deltaTime);
+    // }
+    //
+    // public interface IFixedUpdate<E> : IBehaviour<E> where E : IEntity<E>
+    // {
+    //     void OnFixedUpdate(IEntity<E> entity, float deltaTime);
+    // }
+    //
+    // public interface ILateUpdate<E> : IBehaviour<E> where E : IEntity<E>
+    // {
+    //     void OnLateUpdate(IEntity<E> entity, float deltaTime);
+    // }
+    //
+    // public interface IDisable<E> : IBehaviour<E> where E : IEntity<E>
+    // {
+    //     void Disable(IEntity<E> entity);
+    // }
+    //
+    // public interface IDispose<E> where E : IEntity<E>
+    // {
+    //     void Dispose(IEntity<E> entity);
+    // }
+    //
+    // public interface IGizmos<E> : IBehaviour<E> where E : IEntity<E>
+    // {
+    //     void OnGizmosDraw(IEntity<E> entity);
+    // }
 
     #endregion
 }
