b8e199172b958a324736c4d117191801c5d55124
starkre <gulkin.igor.developer@gmail.com>
2025-08-26 20:25:23 +0700

    Подкручиваем!

--- Assets/Plugins/Atomic/Entities/Scripts/Utils/EntityUtils.cs
+++ Assets/Plugins/Atomic/Entities/Scripts/Utils/EntityUtils.cs
@@ -30,10 +30,9 @@ namespace Atomic.Entities
                 return PrimeTable[index];
 
             index = ~index;
-            if (index < PrimeTable.Length)
-                return PrimeTable[index];
-
-            throw new InvalidOperationException($"Prime can't get larger than {PrimeTable.Length - 1}");
+            return index < PrimeTable.Length
+                ? PrimeTable[index]
+                : throw new InvalidOperationException($"Prime can't get larger than {PrimeTable.Length - 1}");
         }
 
         /// <summary>
@@ -67,47 +66,6 @@ namespace Atomic.Entities
         /// </summary>
         internal static bool IsRunInEditModeDefined(object obj) =>
             obj.GetType().IsDefined(typeof(RunInEditModeAttribute));
-
-        // /// <summary>
-        // /// Computes the next prime number greater than or equal to the specified value.
-        // /// </summary>
-        // /// <param name="number">The number to evaluate.</param>
-        // /// <returns>A prime number greater than or equal to <paramref name="number"/>.</returns>
-        // [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        // internal static int GetPrime(int number)
-        // {
-        //     if (number < 0)
-        //         throw new ArgumentOutOfRangeException(nameof(number));
-        //
-        //     if (number == 0)
-        //         return 1;
-        //
-        //     if (number <= 3)
-        //         return number;
-        //
-        //     while (!IsPrime(number))
-        //         number++;
-        //
-        //     return number;
-        // }
-
-        // /// <summary>
-        // /// Determines whether the given number is prime.
-        // /// </summary>
-        // /// <param name="number">The number to test.</param>
-        // /// <returns><c>true</c> if the number is prime; otherwise, <c>false</c>.</returns>
-        // [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        // internal static bool IsPrime(int number)
-        // {
-        //     if (number % 2 == 0 || number % 3 == 0)
-        //         return false;
-        //
-        //     for (int i = 5; i * i <= number; i += 2)
-        //         if (number % i == 0)
-        //             return false;
-        //
-        //     return true;
-        // }
 
         /// <summary>
         /// Adds an item to a dynamically managed array, expanding it if needed.
@@ -221,65 +179,6 @@ namespace Atomic.Entities
             }
 
             return false;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal static bool FindIndex<T>(T[] array, int count, T item, IEqualityComparer<T> comparer, out int i)
-        {
-            for (i = 0; i < count; i++)
-                if (comparer.Equals(array[i], item))
-                    return true;
-
-            i = -1;
-            return false; // если не найден
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal static bool RemoveAt<T>(ref T[] array, ref int count, int index)
-        {
-            if (index < 0 || index >= count)
-                return false;
-
-            count--;
-
-            for (int i = index; i < count; i++)
-                array[i] = array[i + 1];
-
-            array[count] = default;
-            return true;
-        }
-
-        /// <summary>
-        /// Computes a 32-bit FNV-1a hash for the specified input string.
-        /// </summary>
-        /// <remarks>
-        /// This implementation uses the Fowler–Noll–Vo hash function (FNV-1a), which offers good distribution
-        /// and performance for hashing text. It is not cryptographically secure and is intended for use cases
-        /// such as dictionaries, hash sets, quick comparisons, and ID generation.
-        /// </remarks>
-        /// <param name="input">The input string to hash. Must not be null.</param>
-        /// <returns>
-        /// A 32-bit signed integer hash representing the input string.
-        /// </returns>
-        /// <exception cref="ArgumentNullException">
-        /// Thrown when <paramref name="input"/> is <c>null</c>.
-        /// </exception>
-        public static int StrongFnv1aHash(string input)
-        {
-            unchecked
-            {
-                const uint fnvOffsetBasis = 2166136261;
-                const uint fnvPrime = 16777619;
-                uint hash = fnvOffsetBasis;
-
-                for (int i = 0, count = input.Length; i < count; i++)
-                {
-                    hash ^= input[i];
-                    hash *= fnvPrime;
-                }
-
-                return (int) hash;
-            }
         }
     }
 }
