5143127eb6b2a3268b8aefb461b5d2ae92f20f47
Igor Gulkin <156682538+StarKRE22@users.noreply.github.com>
2026-08-05 22:24:42 +0700

    Update Extensions.cs

--- Assets/Plugins/Atomic/Elements/Scripts/Expressions/Extensions.cs
+++ Assets/Plugins/Atomic/Elements/Scripts/Expressions/Extensions.cs
@@ -4,10 +4,21 @@ using System.Runtime.CompilerServices;
 
 namespace Atomic.Elements
 {
+    /// <summary>
+    /// Provides extension methods for working with expressions and enumerable collections.
+    /// </summary>
     public partial class Extensions
     {
         #region Sum
 
+        /// <summary>
+        /// Calculates the sum of all values in the sequence.
+        /// </summary>
+        /// <param name="list">The sequence of floating-point values.</param>
+        /// <returns>The sum of all elements.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="list"/> is <see langword="null"/>.
+        /// </exception>       
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static float Sum(this IEnumerable<float> list)
         {
@@ -21,6 +32,14 @@ namespace Atomic.Elements
             return result;
         }
 
+        /// <summary>
+        /// Calculates the sum of all values in the sequence.
+        /// </summary>
+        /// <param name="list">The sequence of integer values.</param>
+        /// <returns>The sum of all elements.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="list"/> is <see langword="null"/>.
+        /// </exception>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static float Sum(this IEnumerable<int> list)
         {
@@ -37,7 +56,15 @@ namespace Atomic.Elements
         #endregion
 
         #region Mul
-
+        
+        /// <summary>
+        /// Multiplies all values in the sequence.
+        /// </summary>
+        /// <param name="list">The sequence of floating-point values.</param>
+        /// <returns>The product of all elements.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="list"/> is <see langword="null"/>.
+        /// </exception>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static float Multiply(this IEnumerable<float> list)
         {
@@ -51,6 +78,14 @@ namespace Atomic.Elements
             return result;
         }
 
+        /// <summary>
+        /// Multiplies all values in the sequence.
+        /// </summary>
+        /// <param name="list">The sequence of floating-point values.</param>
+        /// <returns>The product of all elements.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="list"/> is <see langword="null"/>.
+        /// </exception>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static float Multiply(this IEnumerable<int> list)
         {
@@ -68,6 +103,17 @@ namespace Atomic.Elements
 
         #region And
 
+        /// <summary>
+        /// Returns <see langword="true"/> if every value in the sequence is <see langword="true"/>.
+        /// </summary>
+        /// <param name="list">The sequence of boolean values.</param>
+        /// <returns>
+        /// <see langword="true"/> if all elements are <see langword="true"/> or the sequence is empty;
+        /// otherwise, <see langword="false"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="list"/> is <see langword="null"/>.
+        /// </exception>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool And(this IEnumerable<bool> list)
         {
@@ -85,6 +131,16 @@ namespace Atomic.Elements
 
         #region Or
 
+        /// <summary>
+        /// Returns <see langword="true"/> if at least one value in the sequence is <see langword="true"/>.
+        /// </summary>
+        /// <param name="list">The sequence of boolean values.</param>
+        /// <returns>
+        /// <see langword="true"/> if any element is <see langword="true"/>; otherwise, <see langword="false"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="list"/> is <see langword="null"/>.
+        /// </exception>    
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool Or(this IEnumerable<bool> list)
         {
@@ -102,6 +158,14 @@ namespace Atomic.Elements
 
         #region Extensions
 
+        /// <summary>
+        /// Adds a function associated with the specified source object.
+        /// </summary>
+        /// <typeparam name="R">The expression result type.</typeparam>
+        /// <param name="it">The target expression.</param>
+        /// <param name="source">The owner of the function.</param>
+        /// <param name="func">The function to add.</param>
+        /// <returns>The same expression instance.</returns>    
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IExpression<R> Add<R>(this IExpression<R> it, object source, Func<R> func)
         {
@@ -109,13 +173,19 @@ namespace Atomic.Elements
             return it;
         }
 
+        /// <summary>
+        /// Adds a function object associated with the specified source object.
+        /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IExpression<R> Add<R>(this IExpression<R> it, object source, IFunction<R> func)
         {
             it.Add(new ExpressionMember<R>(source, func.Invoke));
             return it;
         }
-        
+
+        /// <summary>
+        /// Adds a function object without specifying a source.
+        /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IExpression<R> Add<R>(this IExpression<R> it, IFunction<R> func)
         {
@@ -123,6 +193,9 @@ namespace Atomic.Elements
             return it;
         }
 
+        /// <summary>
+        /// Adds a source-function pair.
+        /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IExpression<R> Add<R>(this IExpression<R> it, KeyValuePair<object, IFunction<R>> function)
         {
@@ -130,6 +203,9 @@ namespace Atomic.Elements
             return it;
         }
 
+        /// <summary>
+        /// Adds a function without specifying a source.
+        /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IExpression<R> Add<R>(this IExpression<R> it, Func<R> func)
         {
@@ -137,6 +213,12 @@ namespace Atomic.Elements
             return it;
         }
 
+        /// <summary>
+        /// Removes the first expression member associated with the specified source.
+        /// </summary>
+        /// <returns>
+        /// <see langword="true"/> if a member was removed; otherwise, <see langword="false"/>.
+        /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool Remove<R>(this IExpression<R> it, object source)
         {
@@ -152,6 +234,12 @@ namespace Atomic.Elements
             return false;
         }
 
+        /// <summary>
+        /// Removes the first expression member using the specified delegate.
+        /// </summary>
+        /// <returns>
+        /// <see langword="true"/> if a member was removed; otherwise, <see langword="false"/>.
+        /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool Remove<R>(this IExpression<R> it, Func<R> value)
         {
@@ -166,7 +254,13 @@ namespace Atomic.Elements
 
             return false;
         }
-        
+
+        /// <summary>
+        /// Removes the first expression member using the specified function object.
+        /// </summary>
+        /// <returns>
+        /// <see langword="true"/> if a member was removed; otherwise, <see langword="false"/>.
+        /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool Remove<R>(this IExpression<R> it, IFunction<R> value) => 
             it.Remove(value.Invoke);
@@ -181,6 +275,9 @@ namespace Atomic.Elements
             return it;
         }
 
+        /// <summary>
+        /// Adds a binary function associated with the specified source object.
+        /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IExpression<T1, T2, R> Add<T1, T2, R>(
             this IExpression<T1, T2, R> it,
@@ -191,6 +288,9 @@ namespace Atomic.Elements
             return it;
         }
 
+        /// <summary>
+        /// Adds a binary function object associated with the specified source object.
+        /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IExpression<T1, T2, R> Add<T1, T2, R>(
             this IExpression<T1, T2, R> it,
@@ -200,6 +300,9 @@ namespace Atomic.Elements
             return it;
         }
 
+        /// <summary>
+        /// Adds a source-function pair.
+        /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IExpression<T1, T2, R> Add<T1, T2, R>(
             this IExpression<T1, T2, R> it,
@@ -209,6 +312,9 @@ namespace Atomic.Elements
             return it;
         }
 
+        /// <summary>
+        /// Adds a binary function without specifying a source.
+        /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool Remove<T1, T2, R>(
             this IExpression<T1, T2, R> it,
@@ -226,6 +332,9 @@ namespace Atomic.Elements
             return false;
         }
 
+        /// <summary>
+        /// Removes the first expression member associated with the specified source.
+        /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool Remove<R, T1, T2>(this IExpression<T1, T2, R> it, Func<T1, T2, R> value)
         {
@@ -241,10 +350,16 @@ namespace Atomic.Elements
             return false;
         }
 
+        /// <summary>
+        /// Removes the first expression member using the specified delegate.
+        /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool Remove<T1, T2, R>(this IExpression<T1, T2, R> it, IFunction<T1, T2, R> value) => 
             it.Remove(value.Invoke);
-        
+
+        /// <summary>
+        /// Removes the first expression member using the specified function object.
+        /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IExpression<T1, R> Add<T1, R>(
             this IExpression<T1, R> it,
@@ -255,6 +370,9 @@ namespace Atomic.Elements
             return it;
         }
 
+        /// <summary>
+        /// Adds a unary function associated with the specified source object.
+        /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IExpression<T1, R> Add<T1, R>(
             this IExpression<T1, R> it,
@@ -265,6 +383,9 @@ namespace Atomic.Elements
             return it;
         }
 
+        /// <summary>
+        /// Adds a unary function object associated with the specified source object.
+        /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IExpression<T1, R> Add<T1, R>(
             this IExpression<T1, R> it,
@@ -274,6 +395,9 @@ namespace Atomic.Elements
             return it;
         }
 
+        /// <summary>
+        /// Adds a source-function pair.
+        /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IExpression<T1, R> Add<T1, R>(
             this IExpression<T1, R> it,
@@ -283,6 +407,9 @@ namespace Atomic.Elements
             return it;
         }
 
+        /// <summary>
+        /// Adds a unary function without specifying a source.
+        /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool Remove<T, R>(
             this IExpression<T, R> it,
@@ -300,6 +427,9 @@ namespace Atomic.Elements
             return false;
         }
 
+        /// <summary>
+        /// Removes the first expression member associated with the specified source.
+        /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool Remove<R, T>(this IExpression<T, R> it, Func<T, R> value)
         {
@@ -314,13 +444,19 @@ namespace Atomic.Elements
 
             return false;
         }
-        
+
+        /// <summary>
+        /// Removes the first expression member using the specified delegate.
+        /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool Remove<T, R>(this IExpression<T, R> it, IFunction<T, R> value) => 
             it.Remove(value.Invoke);
 
         #endregion
 
+        /// <summary>
+        /// Removes the first expression member using the specified function object.
+        /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IReadOnlyReactiveList<ExpressionMember<R>>.StateChangedSubscription SubscribeState<R>(
             this IExpression<R> it, Action<R> action
