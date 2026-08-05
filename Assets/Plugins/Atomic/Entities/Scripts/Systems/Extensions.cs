2be1dfcd2e46ee230498a9a918ffb65337d33570
Igor Gulkin <156682538+StarKRE22@users.noreply.github.com>
2026-08-05 23:16:58 +0700

    Update Extensions.cs

--- Assets/Plugins/Atomic/Entities/Scripts/Systems/Extensions.cs
+++ Assets/Plugins/Atomic/Entities/Scripts/Systems/Extensions.cs
@@ -2,6 +2,13 @@ namespace Atomic.Entities
 {
     public static partial class Extensions
     {
+        /// <summary>
+        /// Registers an entity system that is updated during the Tick phase of the context entity lifecycle.
+        /// </summary>
+        /// <typeparam name="TContext">Type of the context entity.</typeparam>
+        /// <typeparam name="TEntity">Type of entities processed by the system.</typeparam>
+        /// <param name="entity">The context entity.</param>
+        /// <param name="system">The entity system to register.</param>    
         public static void AddTickSystem<TContext, TEntity>(this TContext entity, EntitySystemBase<TEntity> system)
             where TContext : IEntity
             where TEntity : IEntity
@@ -12,6 +19,13 @@ namespace Atomic.Entities
             entity.WhenDispose(system.Dispose);
         }
 
+        /// <summary>
+        /// Registers an entity system that is updated during the FixedTick phase of the context entity lifecycle.
+        /// </summary>
+        /// <typeparam name="TContext">Type of the context entity.</typeparam>
+        /// <typeparam name="TEntity">Type of entities processed by the system.</typeparam>
+        /// <param name="entity">The context entity.</param>
+        /// <param name="system">The entity system to register.</param>
         public static void AddFixedTickSystem<TContext, TEntity>(this TContext entity, EntitySystemBase<TEntity> system)
             where TContext : IEntity
             where TEntity : IEntity
@@ -22,6 +36,13 @@ namespace Atomic.Entities
             entity.WhenDispose(system.Dispose);
         }
 
+        /// <summary>
+        /// Registers an entity system that is updated during the LateTick phase of the context entity lifecycle.
+        /// </summary>
+        /// <typeparam name="TContext">Type of the context entity.</typeparam>
+        /// <typeparam name="TEntity">Type of entities processed by the system.</typeparam>
+        /// <param name="entity">The context entity.</param>
+        /// <param name="system">The entity system to register.</param>
         public static void AddLateTickSystem<TContext, TEntity>(this TContext entity, EntitySystemBase<TEntity> system)
             where TContext : IEntity
             where TEntity : IEntity
