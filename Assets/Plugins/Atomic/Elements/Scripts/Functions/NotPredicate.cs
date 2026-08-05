c9ad908741259d924f94f63b3ec2903b5791107e
Igor Gulkin <156682538+StarKRE22@users.noreply.github.com>
2026-08-05 22:27:00 +0700

    Update NotPredicate.cs

--- Assets/Plugins/Atomic/Elements/Scripts/Functions/NotPredicate.cs
+++ Assets/Plugins/Atomic/Elements/Scripts/Functions/NotPredicate.cs
@@ -8,15 +8,30 @@ using UnityEngine;
 
 namespace Atomic.Elements
 {
+    /// <summary>
+    /// Represents a predicate that returns the logical negation
+    /// of another predicate.
+    /// </summary>
     [Serializable]
     public sealed class NotPredicate : IPredicate
     {
 #if ODIN_INSPECTOR
         [HideLabel]
 #endif
+        /// <summary>
+        /// The predicate to negate.
+        /// </summary>
         [SerializeReference]
         private IPredicate condition;
-        
+
+        /// <summary>
+        /// Evaluates the predicate and returns its logical negation.
+        /// </summary>
+        /// <returns>
+        /// <see langword="true"/> if the assigned predicate evaluates to
+        /// <see langword="false"/>; otherwise, <see langword="false"/>.
+        /// Returns <see langword="false"/> if no predicate is assigned.
+        /// </returns>
         public bool Invoke()
         {
             return this.condition != null && !this.condition.Invoke();
